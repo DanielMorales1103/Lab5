54bb51df4d3536791d9b3d130bee92a4057f8186
DanielMorales1103 <mor21785@uvg.edu.gt>
2022-08-16 22:24:22 -0600

    Create Restaurante.kt

---
+++ lab4/app/src/main/java/com/example/lab4/data/Restaurante.kt
@@ -0,0 +1,11 @@
+package com.example.lab4.data
+
+import java.io.Serializable
+
+data class Restaurante(
+    val nombre: String,
+    val direccion: String,
+    val horario: String
+): Serializable
+
+
